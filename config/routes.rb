6bc90e084706c492bb836538031647bef41ed042
Leestan360 <stanleymayore2@gmail.com>
2022-10-10 22:53:49 +0300

    Serialize user data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # resources :users, only: [:destroy]
+  # resources :users, only: [:destroy, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
