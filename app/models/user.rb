fe66093352f3d77828e35981683d558556813955
Leestan360 <stanleymayore2@gmail.com>
2022-10-10 15:52:31 +0300

    Create signup route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  has_secure_password
+  
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  
 end
