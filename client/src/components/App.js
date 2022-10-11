c84133b061801fbda1ea0bd52f1254c624fcad96
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-10-11 12:44:19 +0300

    Create and style a signup component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./Home"
+import SignupForm from "./signupForm";
 
 function App() {
 
   return (
     <div>
       <Home/>
+      <SignupForm/>
     </div>
   )
 }
