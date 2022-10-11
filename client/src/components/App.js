b8771b76dd11893e59a0a8b3511274e0313f827e
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-11 04:09:05 +0300

    Adds the courses component which gives a preview of an enrolled course and displays all the courses

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Courses from "./Courses";
 
 function App() {
-
   return (
     <div>
       <p className="text-green-400">Project</p>
+      <Courses />
     </div>
-  )
+  );
 }
 
 export default App;
