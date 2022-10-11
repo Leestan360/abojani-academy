3e599264a2a33adee7f3b610def5f96e5a58e492
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-10-11 20:56:03 +0300

    Add the stock recommendations in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import pic1 from "./images/image.png"
 import data from "../data"
 import news from "../news"
+import { HiOutlineStar } from "react-icons/hi";
 
 
 function Home() {
@@ -10,7 +11,7 @@ function Home() {
         <img className="w-full rounded-md" src={pic1} alt="" />
 
         <div>
-            <h2 className="text-2xl font-medium my-4">Explore courses</h2>
+            <h2 className="text-2xl font-medium my-4">Explore courses <span className="float-right text-[20px] font-light underline">see all</span></h2>
             <div className="courses-images-div">
                 {data.map((item)=> (
                     <div key={item.id} className="one-fetched-course-div">
@@ -27,7 +28,7 @@ function Home() {
 
         <div className="community-topics-div">
             <div style={{width: "33%"}}>
-                <h3 className="text-[#575757] text-2xl font-medium mb-4">Trending News</h3>
+                <h3 className="text-[#575757] text-xl font-medium mb-4">Trending News</h3>
                 <div className="" style={{}}>
                     {news.map((items) => (
                         <div className="flex border mb-4 p-4 rounded-md fetched-div" key={items.id}>
@@ -45,21 +46,56 @@ function Home() {
             </div>
 
             <div style={{width: "64%"}}>
-                <h3 className="text-[#575757] text-2xl font-medium mb-4">Stock Recommendations</h3>
-                <div className="" style={{}}>
-                    {news.map((items) => (
-                        <div className="flex border mb-4 p-4 rounded-md" key={items.id}>
-                            <div style={{width: "75%"}} className="mr-2">
-                                <p>{items.newsletter}</p>
-                                <p className="font-bold text-lg">{items.header}</p>
-                                <p>{items.time}</p>
-                            </div>
-                            <div style={{width: "25%"}}>
-                                <img className="rounded-md float-right" src={items.image} alt="" />
-                            </div>
+                <h3 className="text-[#575757] text-xl font-medium mb-4">Stock Recommendations</h3>
+
+                <div className="py-4 px-8 bg-[#fff] mb-8 rounded-md stock-recommendations">
+                    <div className="flex justify-between items-center mb-4">
+                        <img src="https://elvis-munene.github.io/Abojani-Academy-web-app/assets/images/Ellipse%20(1).png" alt="" />
+                        <div className="w-5/6">
+                            <h2 className="text-2xl font-medium">Cannabis Stocks</h2>
+                            <p><span className="text-base font-bold text-[#40b551]">+2.90%</span> <span className="ml-8 font-bold">21 Symbols</span></p>
                         </div>
-                    ))}
+                        <div><HiOutlineStar className="text-2xl"/></div>
+                    </div>
+                    <p>The JSE has announced the deployment of an online trading analytics platform for listed firms and exchange traded funds(ETFs). This software has been provided</p>
                 </div>
+                
+                <div className="py-4 px-8 bg-[#fff] mb-8 rounded-md stock-recommendations">
+                    <div className="flex justify-between items-center mb-4">
+                        <img src="https://elvis-munene.github.io/Abojani-Academy-web-app/assets/images/Ellipse%20(1).png" alt="" />
+                        <div className="w-5/6">
+                            <h2 className="text-2xl font-medium">Cannabis Stocks</h2>
+                            <p><span className="text-base font-bold text-[#40b551]">+2.90%</span> <span className="ml-8 font-bold">21 Symbols</span></p>
+                        </div>
+                        <div><HiOutlineStar className="text-2xl"/></div>
+                    </div>
+                    <p>The JSE has announced the deployment of an online trading analytics platform for listed firms and exchange traded funds(ETFs). This software has been provided</p>
+                </div>
+
+                <div className="py-4 px-8 bg-[#fff] mb-8 rounded-md stock-recommendations">
+                    <div className="flex justify-between items-center mb-4">
+                        <img src="https://elvis-munene.github.io/Abojani-Academy-web-app/assets/images/Ellipse%20(1).png" alt="" />
+                        <div className="w-5/6">
+                            <h2 className="text-2xl font-medium">Cannabis Stocks</h2>
+                            <p><span className="text-base font-bold text-[#40b551]">+2.90%</span> <span className="ml-8 font-bold">21 Symbols</span></p>
+                        </div>
+                        <div><HiOutlineStar className="text-2xl"/></div>
+                    </div>
+                    <p>The JSE has announced the deployment of an online trading analytics platform for listed firms and exchange traded funds(ETFs). This software has been provided</p>
+                </div>
+                
+                <div className="py-4 px-8 bg-[#fff] mb-8 rounded-md stock-recommendations">
+                    <div className="flex justify-between items-center mb-4">
+                        <img src="https://elvis-munene.github.io/Abojani-Academy-web-app/assets/images/Ellipse%20(1).png" alt="" />
+                        <div className="w-5/6">
+                            <h2 className="text-2xl font-medium">Cannabis Stocks</h2>
+                            <p><span className="text-base font-bold text-[#40b551]">+2.90%</span> <span className="ml-8 font-bold">21 Symbols</span></p>
+                        </div>
+                        <div><HiOutlineStar className="text-2xl"/></div>
+                    </div>
+                    <p>The JSE has announced the deployment of an online trading analytics platform for listed firms and exchange traded funds(ETFs). This software has been provided</p>
+                </div>
+                
             </div>
 
         </div>
