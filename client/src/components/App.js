5165749f2939dd61f01caccd3aa8976868fcb797
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-10-11 22:55:49 +0300

    Created and styled the Welcome screen component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import WelcomeScreen from "./welcomeScreen";
+
 
 function App() {
 
   return (
-    <div>
-      <p className="text-green-400">Project</p>
+    <div >
+      {/* <p className="text-green-500">Project</p> */}
+      <WelcomeScreen />
+     
     </div>
-  )
+      
+
+  );
 }
 
 export default App;
+
+
---
+++ client/src/components/welcomeScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function WelcomeScreen(){
+  return(
+    <div>
+        <div className="h-full w-full flex welcome-screen-div-one">
+          <div className="w-2/4 ">
+            <img src="https://github.com/Elvis-Munene/Abojani-Academy-web-app/blob/main-page/assets/images/main-image-signup.png?raw=true" alt=""/>
+          
+          </div>
+            <div className="bg-green-500 text-white w-2/4 h-screen  grid-rows-1 grid-cols-1 grid ">
+            <div className="border border-black max-w-lg p-8 bg-white grid w-3/5 m-40 rounded-xl" >
+              <h2 className=" w-36 h-10 left-5/6 top-56 text-3xl pb-5 text-black" >
+                Welcome
+              </h2>
+              <h1 className="text-4xl font-semibold text-green-700 pb-8">Abojani <span className="text-amber-600">Academy</span></h1>
+              <p className="text-base text-black pb-5 leading-6 ">
+                We shorten the time between learning and investing and saving by connecting our clients
+              </p>
+              <button className="bg-amber-600 py-2 px-2 text-2xl rounded cursor-pointer shadow-slate-400 border-none m-7 ">Log in</button> 
+              <button className="bg-white py-1 px-2 text-2xl rounded cursor-pointer text-amber-600 shadow-slate-400 border border-solid border-amber-600 m-0">Create Account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    
+
+
+  )
+}
+export default WelcomeScreen;
+
+
+
+
