f991ce0a838842b1e208ec6c9877526dd9d9e2c3
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-12 16:43:04 +0300

    Refactoring

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import CommunityInformation from "./CommunityInformation.js";
+import Courses from "./Courses";
 
+function App() {
+  return (
+    <div>
+      <p className="text-green-400">Project</p>
+      <Courses />
+      <CommunityInformation />
+    </div>
+  );
+}
+
+export default App;
