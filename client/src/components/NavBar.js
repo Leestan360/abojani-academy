f70bc26cf753415486469cba6ce9f2224b2610c2
Leestan360 <stanleymayore2@gmail.com>
2022-10-13 13:59:24 +0300

    Fetch current user

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { BsCart2, BsBell } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
-function NavBar({user}) {
+function NavBar({ user }) {
   return (
     <div
       className="flex justify-between h-16 w-screen fixed bg-white"
@@ -31,9 +33,16 @@ function NavBar({user}) {
         </span>
       </h3>
       <div className="flex text-xl">
-        <p className="mx-4 my-auto">Cart</p>
-        <p className="mx-4 my-auto">Notifications</p>
-        <p className="mr-24 ml-4 my-auto">{}</p>
+        <p className="mx-4 my-auto">
+          {" "}
+          <BsCart2 className="hover:text-green-500 cursor-pointer " />
+        </p>
+        <p className="mx-4 my-auto">
+          <BsBell className="hover:text-orange-500 cursor-pointer" />
+        </p>
+        <p className="mr-12 ml-4 my-auto flex items-center text-2xl">
+          <FaUser className="mr-1"/> {user.username}
+        </p>
       </div>
     </div>
   );
