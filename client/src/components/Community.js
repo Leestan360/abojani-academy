ed848f85c2dc90ad95366e00706a605ecc4fd49c
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-14 12:59:22 +0300

    Adds routing to the community information

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -1,63 +1,167 @@
-import { IoIosCheckmarkCircle } from "react-icons/io"
+import { IoIosCheckmarkCircle } from "react-icons/io";
 import { AiOutlineStop } from "react-icons/ai";
+import CommunityInformation from "./CommunityInformation.js";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 
 function Community() {
+  let navigate = useNavigate();
 
-    return (
-      <div className="community w-3/4 pt-32 ml-80">
-        <div className="community-sections-div">
-            <h1 className="text-[#f17425] font-bold text-base mb-2">INNER CIRCLE</h1>
-            <p className="text-2xl font-bold mb-4">KES 13,000</p>
-            <div className="p-4 w-full bg-[#e7e7e7] rounded-md mb-6">
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/> Cash Flow & Debt Management</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Investments Advice</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Daily & Weekly Financial Reports</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Weekly Stock Recommendation</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Insurance Planning</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Portfolio Review & Balancing</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Estate Planning</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Personal Accountability Manager</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Investments Advice</p>
-            </div>
-            <button className="px-6 py-3 bg-[#f17425] rounded-md text-[#fff]">Join Now</button>
-        </div>
-
-        <div className="community-sections-div">
-            <h1 className="text-[#f17425] font-bold text-base mb-2">HIDDEN GEM</h1>
-            <p className="text-2xl font-bold mb-4">KES 24,000</p>
-            <div className="p-4 w-full bg-[#e7e7e7] rounded-md mb-6">
-            <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/> Cash Flow & Debt Management</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Investments Advice</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Daily & Weekly Financial Reports</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Weekly Stock Recommendation</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Insurance Planning</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Portfolio Review & Balancing</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Estate Planning</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Personal Accountability Manager</p>
-                <p className="m-2 flex content-center"><AiOutlineStop className="text-[#FF0000] text-xl mr-2"/>Investments Advice</p>
-            </div>
-            <button className="px-6 py-3 bg-[#f17425] rounded-md text-[#fff]">Join Now</button>
-        </div>
-
-        <div className="community-sections-div">
-            <h1 className="text-[#f17425] font-bold text-base mb-2">MASTER MIND</h1>
-            <p className="text-2xl font-bold mb-4">KES 35,000</p>
-            <div className="p-4 w-full bg-[#e7e7e7] rounded-md mb-6">
-            <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/> Cash Flow & Debt Management</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Investments Advice</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Daily & Weekly Financial Reports</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Weekly Stock Recommendation</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Insurance Planning</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Portfolio Review & Balancing</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Estate Planning</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Personal Accountability Manager</p>
-                <p className="m-2 flex content-center"><IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2"/>Investments Advice</p>
-            </div>
-            <button className="px-6 py-3 bg-[#f17425] rounded-md text-[#fff]">Join Now</button>
-        </div>
-        
-      </div>
-    )
+  function handleJoin() {
+    navigate("/communityinformation");
   }
-  
-  export default Community;
+
+  return (
+    <div className="community w-3/4 pt-32 ml-80">
+      <div className="community-sections-div">
+        <h1 className="text-[#f17425] font-bold text-base mb-2">
+          INNER CIRCLE
+        </h1>
+        <p className="text-2xl font-bold mb-4">KES 13,000</p>
+        <div className="p-4 w-full bg-[#e7e7e7] rounded-md mb-6">
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />{" "}
+            Cash Flow & Debt Management
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Investments Advice
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Daily & Weekly Financial Reports
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Weekly Stock Recommendation
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Insurance Planning
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Portfolio Review & Balancing
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Estate Planning
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Personal Accountability Manager
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Investments Advice
+          </p>
+        </div>
+        <Link to="/communityinformation">
+          <button className="px-6 py-3 bg-[#f17425] rounded-md text-[#fff]">
+            Join Now
+          </button>
+        </Link>
+      </div>
+
+      <div className="community-sections-div">
+        <h1 className="text-[#f17425] font-bold text-base mb-2">HIDDEN GEM</h1>
+        <p className="text-2xl font-bold mb-4">KES 24,000</p>
+        <div className="p-4 w-full bg-[#e7e7e7] rounded-md mb-6">
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />{" "}
+            Cash Flow & Debt Management
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Investments Advice
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Daily & Weekly Financial Reports
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Weekly Stock Recommendation
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Insurance Planning
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Portfolio Review & Balancing
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Estate Planning
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Personal Accountability Manager
+          </p>
+          <p className="m-2 flex content-center">
+            <AiOutlineStop className="text-[#FF0000] text-xl mr-2" />
+            Investments Advice
+          </p>
+        </div>
+        <button className="px-6 py-3 bg-[#f17425] rounded-md text-[#fff]">
+          Join Now
+        </button>
+      </div>
+
+      <div className="community-sections-div">
+        <h1 className="text-[#f17425] font-bold text-base mb-2">MASTER MIND</h1>
+        <p className="text-2xl font-bold mb-4">KES 35,000</p>
+        <div className="p-4 w-full bg-[#e7e7e7] rounded-md mb-6">
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />{" "}
+            Cash Flow & Debt Management
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Investments Advice
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Daily & Weekly Financial Reports
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Weekly Stock Recommendation
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Insurance Planning
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Portfolio Review & Balancing
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Estate Planning
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Personal Accountability Manager
+          </p>
+          <p className="m-2 flex content-center">
+            <IoIosCheckmarkCircle className="text-[#40b551] text-xl mr-2" />
+            Investments Advice
+          </p>
+        </div>
+        <button className="px-6 py-3 bg-[#f17425] rounded-md text-[#fff]">
+          Join Now
+        </button>
+      </div>
+      <Routes>
+        <Route
+          exact
+          path="/communityinformation"
+          element={<CommunityInformation />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default Community;
