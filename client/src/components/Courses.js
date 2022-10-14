1086fac1fb1f90424007fc2681bb3df39013470e
Leestan360 <stanleymayore2@gmail.com>
2022-10-14 11:05:14 +0300

    update courses

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import SingleCourse from "./SingleCourse";
 
 function Courses() {
+  
   const [courses, setCourses] = useState([]);
   useEffect(() => {
     fetch("/courses")
